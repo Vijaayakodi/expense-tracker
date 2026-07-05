cc00f24e64f3ec6ceb6616e1032c27bbe95ab973
Vijaayakodi <vijaayakodigl@gmail.com>
2026-07-05 23:35:52 +0530

    Use Render API URL

--- backend/static/script.js
+++ backend/static/script.js
@@ -3,7 +3,7 @@
 // =============================
 
 // Backend API URL
-const API_URL ="http://127.0.0.1:5000";
+const API_URL ="https://expense-tracker-yhve.onrender.com";
 
 // Wait until page loads
 document.addEventListener("DOMContentLoaded", () => {
